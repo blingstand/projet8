fe2af0bbc775c49e2e5265bdf94090729799aeef
adrien Clupot <adrien.clupot@gmail.com>
2020-03-02 09:52:24 +0100

    as a client I can find the website with my browser

---
+++ coach/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+def index(request):
+    # message = "Bienvenu sur le site pureBeurre !"
+    print("\n"*20)
+    print("* * *  "* 20)
+    context = {
+    	'connected' : True}
+    return render(request, 'coach/index.html', context)

60808b65b1eeff097f1d2976177427aae234880e
adrien Clupot <adrien.clupot@gmail.com>
2020-03-16 12:01:37 +0100

    us5 : organise css for input simple_search

--- research/form.py
+++ research/form.py
@@ -2,4 +2,7 @@ from django import forms
 
 
 class SearchForm(forms.Form):
-    simple_search = forms.CharField(label="Entrez le nom d'un produit", max_length=50)
+    simple_search = forms.CharField(
+    	label="Entrez le nom d'un produit", 
+    	max_length=50,
+    	widget=forms.TextInput(attrs={'id': 'simple_form_input'}) )

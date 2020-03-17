f6bee8aa805c255ff6522bed16b1c55902c0571a
adrien Clupot <adrien.clupot@gmail.com>
2020-03-17 13:11:35 +0100

    us5 : create a result page

--- research/urls.py
+++ research/urls.py
@@ -5,4 +5,7 @@ from . import views as v # import views so we can use them in urls.
 
 urlpatterns = [
     path(r'advancedSearch', v.AdvancedSearchView.as_view(), name="advancedSearch"),
+    path(r'results', v.ResultsView.as_view(), name="results")
+
+
 ]
--- research/views.py
+++ research/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.views import View
 
 from .form import SearchForm, AdvancedSearchForm
@@ -16,4 +17,19 @@ class AdvancedSearchView(View):
 		return render(request, "research/advancedSearch.html", context)
 
 	def post(self, request):
-		pass
+		form = AdvancedSearchForm(request.POST)
+		results=(1,2,3)
+		if form.is_valid():
+			results = [r for r in range(10)]
+			context = { "results" : results}
+			return render(request, "research/results.html", context)
+		return HttpResponse("Pb dans le form")
+
+class ResultsView(View):
+	""" manage a HttpResponse in case of get or post method 
+	request for this url research/advancedSearch"""
+
+	def get(self, request):
+		""" manage the HttpResponse for the SearchFormView with get method request """
+		return render(request, "research/results.html")
+

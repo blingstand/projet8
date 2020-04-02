4951c570f6977c5aa53676e48a8589134a30211a
adrien Clupot <adrien.clupot@gmail.com>
2020-04-02 11:41:16 +0200

    us 7 : create a form to add favorite in db

--- user/form.py
+++ user/form.py
@@ -10,3 +10,9 @@ class MoreUserDataForm(forms.Form):
 	mail = forms.EmailField(			label="Mon mail", 
 										max_length=100, 
 										widget=forms.TextInput(attrs={'class': 'account_i'}))
+
+class AddFavorite(forms.form): 
+	fav = forms.MultipleChoiceField(	
+		required=False,
+        label="ajouter aux favoris",
+        widget=forms.CheckboxSelectMultiple)

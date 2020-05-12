96e0b5ee7ee507dcba0fcdd22548d1d67fbb68ca
adrien Clupot <adrien.clupot@gmail.com>
2020-05-12 08:11:36 +0200

    us12 : try to fix travis error with  geckodriver

--- test/test_us.py
+++ test/test_us.py
@@ -15,7 +15,6 @@ class AccountTestCase(LiveServerTestCase):
 
     def test_user_stories(self):
         print("\n\n***\ndébut des test\n***")
-        #self.driver = webdriver.Firefox('/home/blingstand/Bureau/openclassrooms/projets/projet8/pureBeurre/')
         self.driver = webdriver.Firefox()
         self.user = User(username="test")
         self.user.set_password("123")

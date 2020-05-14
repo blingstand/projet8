be8b0376396795e6be9b074e04d62ba3809b1e3e
adrien Clupot <adrien.clupot@gmail.com>
2020-05-14 11:01:51 +0200

    us12 : add sentry

--- pureBeurre/settings/__init__.py
+++ pureBeurre/settings/__init__.py
@@ -12,7 +12,23 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import django_heroku
+import sentry_sdk
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://5086d194ae6846ed847ad9dd66948eb3@o388096.ingest.sentry.io/5224413",
+    integrations=[DjangoIntegration()],
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
+
+# SECURITY WARNING: don't run with debug turned on in production!
+
+DEBUG = False 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,9 +36,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '#y-ca+0y#ewq-i=1y*5_fnad8@&r$t%7g@u#$lmg@2k_tu8cms'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-
-DEBUG = True 
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
